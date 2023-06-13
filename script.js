319fa48bcd53db692e19e8e9613f127edc43c682
Nicole Stark <nicolestark@Nicoles-iMac.attlocal.net>
2023-06-12 20:27:43 -0500

    finished javascript and added comments

--- script.js
+++ script.js
@@ -1,18 +1,61 @@
-// declare variables
 var wins = 0;
-var losses = 0;
 var ties = 0;
+var losses = 0;
 
-// declare the options
-var options = ['R', 'P', 'S'];
+// Array of options for computer to pick from
+var options = ["R", "P", "S"];
 
-// create game function
 var playGame = function() {
-    // prompt user for choice
-    var userChoice = window.prompt("Choose R, P, or S: ");
+  // Ask user for their choice
+  var userChoice = window.prompt("Enter R, P, or S:");
 
-    // if user hits cancel end function
-    if(!userChoice) {
-        return;
-    }
-}
+  // If user pressed Cancel, immediately end function
+  if (!userChoice) {
+    return;
+  }
+
+  // Convert to uppercase to make comparisons easier
+  userChoice = userChoice.toUpperCase();
+
+  // Get random index from array of options
+  var index = Math.floor(Math.random() * options.length);
+  var computerChoice = options[index];
+
+  window.alert("The computer chose " + computerChoice);
+
+  // If choices are the same, it's a tie
+  if (userChoice === computerChoice) {
+    ties++;
+    window.alert("It's a tie!");
+
+  // Check every win condition for the player
+  } else if (
+    (userChoice === "R" && computerChoice === "S") || 
+    (userChoice === "P" && computerChoice === "R") || 
+    (userChoice === "S" && computerChoice === "P")
+  ) {
+    wins++;
+    window.alert("You win!");
+
+  // If above conditions failed, assume player lost
+  } else {
+    losses++;
+    window.alert("You lost!");
+  }
+
+  // Print stats with line breaks
+  window.alert(
+    "Stats:\nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties
+  );
+
+  // Ask user to play again
+  var playAgain = window.confirm("Play again?");
+
+  // If user pressed OK, run the function again
+  if (playAgain) {
+    playGame();
+  }
+};
+
+// Run the game for the first time
+playGame();
